e58cb2fd64ab7ac9f335d188a15669cd47057886
augtal <augustas.talacka@gmail.com>
2020-08-26 15:51:01 +0300

    Added support for 4x4, 6x6, 9x9 size boards

--- app.py
+++ app.py
@@ -27,15 +27,14 @@ def __checkCol(board, cur_pos, number):
             return False
     return True
 
-def __checkGrid(board, cur_pos, number):
+def __checkGrid(board, cur_pos, number, size_row, size_col):
     row, col = cur_pos
-    size = 3
 
-    x_box = row // size #sizeRow   0
-    y_box = col // size #sizeCol   1
+    x_box = row // size_row #sizeRow   0
+    y_box = col // size_col #sizeCol   1
 
-    for x in range(x_box*size, x_box*size+size):
-        for y in range(y_box*size, y_box*size+size):
+    for x in range(x_box*size_row, x_box*size_row+size_row):
+        for y in range(y_box*size_col, y_box*size_col+size_col):
             if x == row and y == col:
                 continue
 
@@ -44,17 +43,17 @@ def __checkGrid(board, cur_pos, number):
 
     return True
 
-def validNumber(board, cur_pos, number):
+def validNumber(board, cur_pos, number, size_row, size_col):
     if not __checkRow(board, cur_pos, number):
         return False
     elif not __checkCol(board, cur_pos, number):
         return False
-    elif not __checkGrid(board, cur_pos, number):
+    elif not __checkGrid(board, cur_pos, number, size_row, size_col):
         return False
     else:
         return True
 
-def solveBoard(board):
+def __solveBoard(board, size_row, size_col):
     empty_cell = findEmpty(board) #returns false if it can't find an empty value
 
     if not empty_cell:
@@ -62,24 +61,36 @@ def solveBoard(board):
     else: 
         row_empty, col_empty = empty_cell
 
-    for i in range(1,10):
-        if(validNumber(board, empty_cell, i)):
+    for i in range(1,len(board)+1):
+        if(validNumber(board, empty_cell, i, size_row, size_col)):
             board[row_empty][col_empty] = i
 
-            if solveBoard(board):
+            if __solveBoard(board, size_row, size_col):
                 return True
 
             board[row_empty][col_empty] = 0 #resets value to try again
 
     return False
 
+def solve(board):
+    size = len(board)
+    if size == 4:
+        __solveBoard(board, int(size/2), int(size/2))
+    elif size == 6:
+        __solveBoard(board, int(size/3), int(size/2))
+    elif size == 9:
+        __solveBoard(board, int(size/3), int(size/3))
+    else:
+        print("Unsupported board")
+
 if __name__=="__main__":
     board4x4, board6x6, board9x9 = board_printer.boards()
-    mainBoard = board9x9
+    mainBoard = board6x6
+
     print("Unsolved grid:")
     board_printer.printBoard(mainBoard)
 
-    solveBoard(mainBoard)
+    solve(mainBoard)
     print(" ")
 
     print("Solved grid:")
