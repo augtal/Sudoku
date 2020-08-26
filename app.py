e7f03896f15297dc2cc00d9a86413ee25a92e391
augtal <augustas.talacka@gmail.com>
2020-08-26 14:58:18 +0300

    Added check row and col methods

--- app.py
+++ app.py
@@ -1,29 +1,53 @@
 import board_printer
 
-def find_empty(board):
+def findEmpty(board):
     for row in range(len(board)):
         for col in range(len(board[row])):
             if board[row][col]==0:
                 return (row,col)
     return (-1,-1)
 
+def checkRow(board, cur_pos):
+    row, col = cur_pos
+    for i in range(len(board[row])):
+        if i == col:
+            continue
 
-def solve_board(board):
-    emptyCells = find_empty(board)
+        if board[row][col] == board[row][i]:
+            return False
+    return True
+
+def checkCol(board, cur_pos):
+    row, col = cur_pos
+    for i in range(len(board[row])):
+        if i == row:
+            continue
+
+        if board[row][col] == board[i][col]:
+            return False
+    return True
 
 
+def solveBoard(board):
+    empty_cell = findEmpty(board)
+    row_empty, col_empty = empty_cell
 
-    print(emptyCells)
+    boardWorking = board
+    boardWorking[row_empty][col_empty] = 2
+
+    print(f"row does't have duplicate: {checkRow(boardWorking, empty_cell)}")
+    print(f"col does't have duplicate: {checkCol(boardWorking, empty_cell)}")
+    #print(f"grid does't have duplicate: {checkGrid(boardWorking, empty_cell)}")
 
 if __name__=="__main__":
     board4x4, board6x6, board9x9 = board_printer.boards()
 
     mainBoard = board4x4
     print("Unsolved grid:")
-    board_printer.print_board(mainBoard)
+    board_printer.printBoard(mainBoard)
 
-    solvedBoard = solve_board(mainBoard)
+    solveBoard(mainBoard)
     print(" ")
 
     print("Solved grid:")
-    board_printer.print_board(mainBoard)
+    board_printer.printBoard(mainBoard)
