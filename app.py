e6fd9230071d8927c8db666697775d28ad8c77c4
augtal <augustas.talacka@gmail.com>
2020-08-26 17:09:56 +0300

    Moved sudoku solving methods to a new file

--- app.py
+++ app.py
@@ -1,88 +1,6 @@
 import board_printer
+import sudoku_solver as solver
 import re
-
-def findEmpty(board):
-    for row in range(len(board)):
-        for col in range(len(board[row])):
-            if board[row][col]==0:
-                return (row,col)
-    return False
-
-def __checkRow(board, cur_pos, number):
-    row, col = cur_pos
-    for i in range(len(board[row])):
-        if i == col:
-            continue
-
-        if number == board[row][i]:
-            return False
-    return True
-
-def __checkCol(board, cur_pos, number):
-    row, col = cur_pos
-    for i in range(len(board[row])):
-        if i == row:
-            continue
-
-        if number == board[i][col]:
-            return False
-    return True
-
-def __checkGrid(board, cur_pos, number, size_row, size_col):
-    row, col = cur_pos
-
-    x_box = row // size_row #sizeRow   0
-    y_box = col // size_col #sizeCol   1
-
-    for x in range(x_box*size_row, x_box*size_row+size_row):
-        for y in range(y_box*size_col, y_box*size_col+size_col):
-            if x == row and y == col:
-                continue
-
-            if number == board[x][y]:
-                return False
-
-    return True
-
-def validNumber(board, cur_pos, number, size_row, size_col):
-    if not __checkRow(board, cur_pos, number):
-        return False
-    elif not __checkCol(board, cur_pos, number):
-        return False
-    elif not __checkGrid(board, cur_pos, number, size_row, size_col):
-        return False
-    else:
-        return True
-
-def __solveBoard(board, size_row, size_col):
-    empty_cell = findEmpty(board) #returns false if it can't find an empty value
-
-    if not empty_cell:
-        return True #found the solution because we have no more empty cells
-    else: 
-        row_empty, col_empty = empty_cell
-
-    for i in range(1,len(board)+1):
-        if(validNumber(board, empty_cell, i, size_row, size_col)):
-            board[row_empty][col_empty] = i
-
-            if __solveBoard(board, size_row, size_col):
-                return True
-
-            board[row_empty][col_empty] = 0 #resets value to try again
-
-    return False
-
-def solve(board):
-    size = len(board)
-    if size == 4:
-        __solveBoard(board, int(size/2), int(size/2))
-    elif size == 6:
-        __solveBoard(board, int(size/3), int(size/2))
-    elif size == 9:
-        __solveBoard(board, int(size/3), int(size/3))
-    else:
-        print("Unsupported board")
 
 def checkInput(number_list, board_size):
     for number in number_list:
@@ -135,7 +53,7 @@ if __name__=="__main__":
             print("Unsolved grid:")
             board_printer.printBoard(mainBoard)
 
-            solve(mainBoard)
+            solver.solve(mainBoard)
             print(" ")
 
             print("Solved grid:")
