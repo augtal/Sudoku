da783cc8eaa4782f406a5751dbf5fa2ce12d00cc
augtal <augustas.talacka@gmail.com>
2020-08-30 14:30:18 +0300

    Added method to paint helper cells

--- GUI.py
+++ GUI.py
@@ -23,6 +23,7 @@ class Cube():
         self.selected = False
         self.temp = None
         self.correct = None
+        self.helper = None
 
     def draw(self, screen, board_size):
         font_size = int(((self.width / board_size) / 2)
@@ -34,6 +35,9 @@ class Cube():
         x = self.col * gap
         y = self.row * gap
 
+        if self.helper:
+            pygame.draw.rect(screen, (245, 245, 245), (x, y, gap, gap))
+
         if self.value != 0 or self.correct:
             cube_text = font.render(str(self.value), 1, (0, 0, 0))
             x_value = gap/2 - cube_text.get_width()/2
@@ -44,8 +48,6 @@ class Cube():
             x_value = gap/2 - cube_text.get_width()/2
             y_value = gap/2 - cube_text.get_height()/2
             screen.blit(cube_text, (x + x_value, y + y_value))
-
-        
 
         if self.selected:
             pygame.draw.rect(screen, (0, 0, 255), (x, y, gap, gap), 3)
@@ -98,6 +100,11 @@ class Board():
     def draw(self, screen):
         gap = self.width / self.size_board
 
+        # draws cube values
+        for x in range(self.size_board):
+            for y in range(self.size_board):
+                self.cubes[x][y].draw(screen, self.size_board)
+
         # draws vertical lines
         for i in range(self.size_board+1):
             if i % self.size_cols == 0 and i != 0:
@@ -114,12 +121,7 @@ class Board():
             else:
                 thick = 1
             pygame.draw.line(screen, (0, 0, 0), (0, j*gap),
-                            (self.width, j*gap), thick)
-
-        # draws cube values
-        for x in range(self.size_board):
-            for y in range(self.size_board):
-                self.cubes[x][y].draw(screen, self.size_board)
+                             (self.width, j*gap), thick)
 
     def click(self, mouse_pos):
         # check if clicked inside the board
@@ -127,26 +129,44 @@ class Board():
             gap = self.width / self.size_board
             x_cord = int(mouse_pos[0] // gap)
             y_cord = int(mouse_pos[1] // gap)
-            return (x_cord, y_cord)
+            return (y_cord, x_cord)
         else:
             return None
 
-    def selectedCell(self, x_cord, y_cord):
-        #first selected cell
+    def setHelperCells(self, y_cord, x_cord, condition):
+        # rows
+        for x in range(self.size_board):
+            self.cubes[y_cord][x].helper = condition
+
+        # cols
+        for y in range(self.size_board):
+            self.cubes[y][x_cord].helper = condition
+
+        x_box = x_cord // self.size_rows
+        y_box = y_cord // self.size_cols
+        for i in range(y_box*self.size_cols, y_box*self.size_cols+self.size_cols):
+            for j in range(x_box*self.size_rows, x_box*self.size_rows+self.size_rows):
+                self.cubes[i][j].helper = condition
+
+    def selectedCell(self, y_cord, x_cord):
+        # first selected cell
         if self.selected_cell is None:
             self.cubes[y_cord][x_cord].selected = True
-            self.selected_cell = (x_cord, y_cord)
-        #other selected cell after the first one
+            self.selected_cell = (y_cord, x_cord)
+            self.setHelperCells(y_cord, x_cord, True)
+        # other selected cell after the first one
         else:
-            old_x, old_y = self.selected_cell
+            old_y, old_x = self.selected_cell
             self.cubes[old_y][old_x].selected = False
             self.cubes[old_y][old_x].correct = None
+            self.setHelperCells(old_y, old_x, False)
 
             self.cubes[y_cord][x_cord].selected = True
-            self.selected_cell = (x_cord, y_cord)
+            self.selected_cell = (y_cord, x_cord)
+            self.setHelperCells(y_cord, x_cord, True)
 
     def placeValue(self, solved_board):
-        x_cord, y_cord = self.selected_cell
+        y_cord, x_cord = self.selected_cell
         value = self.cubes[y_cord][x_cord].temp
 
         if value == solved_board[y_cord][x_cord]:
@@ -157,7 +177,7 @@ class Board():
             self.cubes[y_cord][x_cord].correct = False
 
     def placeTempValue(self, value):
-        x_cord, y_cord = self.selected_cell
+        y_cord, x_cord = self.selected_cell
 
         if self.cubes[y_cord][x_cord].value != 0:
             return
